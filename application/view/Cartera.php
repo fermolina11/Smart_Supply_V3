24aa456e26691a0ab26a4a8232e8223d135648cf
fernando molina <40641236+fermolina11@users.noreply.github.com>
2018-09-01 11:45:31 -0500

    prueba2

--- application/view/Cartera.php
+++ application/view/Cartera.php
@@ -12,7 +12,7 @@
                         ?>
                     </datalist>
         <input type="reset" value="Limpiar" id="limpiar_c">
-        <!-- comentario prueba -->
+    
 
         <div>
             <label>Nombre cliente:</label> <br>
